6fa6e7d7b01a8a11ac52a276f8b7a247556464c9
web-zon <shakilahmadpip3@gmail.com>
2023-06-16 00:32:21 +0600

    this added two num sum and double tio int type cast

---
+++ intro.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    // First Code of print Hello World //
+    printf("Hello Shakil \n");
+
+// Two number calculation //
+    int a = 3;
+    int b = 3;
+    int sum = a + b;
+    printf("sum is %d + %d = %d\n", a, b, sum);
+    printf("sum is %d \n", sum);
+
+// Int to Double Type Cast function //
+    double c = 30.50;
+        int d = (int)c;
+        printf("Double number is %lf \n", c);
+        printf("Double to Int is %d", d);
+        
+    return 0;
+}
